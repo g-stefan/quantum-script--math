8a40ed5ab05888178ce5ad24e7e305fa1675a1d7
Grigore Stefan <g_stefan@yahoo.com>
2021-07-24 19:36:49 +0300

    update

--- source/quantum-script-extension-math-version.cpp
+++ source/quantum-script-extension-math-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace Math {
 				namespace Version {
 
-					static const char *version_ = "1.5.0";
-					static const char *build_ = "6";
-					static const char *versionWithBuild_ = "1.5.0.6";
-					static const char *datetime_ = "2021-07-06 15:11:47";
+					static const char *version_ = "2.0.0";
+					static const char *build_ = "9";
+					static const char *versionWithBuild_ = "2.0.0.9";
+					static const char *datetime_ = "2021-07-23 16:34:02";
 
 					const char *version() {
 						return version_;
